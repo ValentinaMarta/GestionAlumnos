1ebc2ece2fe0ed192f9f07d8d970fcf67ea43f4b
Gabriela Gonzalez <anagabrielagonzalezteran@gmail.com>
2022-07-05 12:47:46 +0200

    cambios html de calificaciones

--- Proyecto_2/webProyecto2app/admin.py
+++ Proyecto_2/webProyecto2app/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from webProyecto2app.models import *
 
 # Register your models here.
+
+
 admin.site.register(Profesor)
 admin.site.register(Alumno)
 admin.site.register(Calificaciones)
